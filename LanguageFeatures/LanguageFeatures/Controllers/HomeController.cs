ec172858074b115ce3b7a7b26199b21a1fa04362
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-01-30 22:00:24 +0300

    Essential Language Features. Using Object and Collection Initializers

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -20,11 +20,35 @@ namespace LanguageFeatures.Controllers
             //create a new Product object
             Product myProduct = new Product();
             //set the property value
-            myProduct.Name = "Jonka";
+            myProduct.Name = "Canoe";
             //get the property
             string productName = myProduct.Name;
             //generate the View
             return View("Result", (object)String.Format("Product name : {0}", productName));
+        }
+
+        public ViewResult CreateProduct()
+        {
+            //create and populate new Product object
+            Product myProduct = new Product
+            {
+                ProductID = 100,
+                Name = "Canoe",
+                Description = "A boat for one person",
+                Price = 3699M,
+                Category = "Watersports"
+            };
+
+            return View("Result", (object)String.Format("Category: {0}; Name: {1}", myProduct.Category, myProduct.Name));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "cucumber" };
+            List<int> intList = new List<int> { 10, 20, 30, 42 };
+            Dictionary<string, int> myDict = new Dictionary<string, int> { { "apple", 10 }, { "orange", 20 }, { "cucumber", 42 } };
+
+            return View("Result", (object)stringArray[2]);
         }
     }
 }
