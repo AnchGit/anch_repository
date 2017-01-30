f468b9bab43eea8138c8246f94d5cdd97eaebda1
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-01-30 21:28:17 +0300

    Essential Language Features. Using Automatically Implemented Properties

--- LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public string Index()
         {
-            return View();
+            return "Navigate to URL to show example.";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            //create a new Product object
+            Product myProduct = new Product();
+            //set the property value
+            myProduct.Name = "Jonka";
+            //get the property
+            string productName = myProduct.Name;
+            //generate the View
+            return View("Result", (object)String.Format("Product name : {0}", productName));
         }
     }
 }
