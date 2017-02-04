985a33bc84b6f61fb152c674580574ed16dc69f6
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-02-04 21:30:30 +0300

    Working with Razor

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        Product myProduct = new Product
+        {
+            ProductID = 1,
+            Name = "Canoe",
+            Description = "A boat for one person",
+            Category = "Watersport",
+            Price = 240M
+        };
+        public ActionResult Index()
+        {
+            return View(myProduct);
+        }
+    }
+}
