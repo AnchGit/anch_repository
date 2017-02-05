21e36b9c2b8533d90108ccc39d35eafc5612f252
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-02-05 16:03:21 +0300

    Essential Tools for MVC. Using Ninject. Setting up MVC Dependency Injection.

--- EssentialTools/EssentialTools/Controllers/HomeController.cs
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -1,12 +1,14 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
 using EssentialTools.Models;
+using Ninject;
 
 namespace EssentialTools.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
+        private IValueCalculator calc;
         private Product[] products =
         {
             new Product { Name = "Canoe", Category = "Watersports", Price = 135000M },
@@ -14,9 +16,22 @@ namespace EssentialTools.Controllers
             new Product { Name = "Tennis ball", Category = "Tennis", Price = 13.50M },
             new Product { Name = "Tennis net", Category = "Tennis", Price = 78.95M }
         };
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
         public ActionResult Index()
         {
-            LinqValueCalculator calc = new LinqValueCalculator();
+            // Basic setting of the Ninject, before setting up MVC Dependency Injection
+            //IKernel ninjectKernel = new StandardKernel();
+            //ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            //IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+
+            // Without Ninject we're still have a problem
+            //IValueCalculator calc = new LinqValueCalculator();
+
             ShoppingCart cart = new ShoppingCart(calc) { Products = products };
             decimal totalValue = cart.CalculateProductTotal();
             return View(totalValue);
