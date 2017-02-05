3dc641293becc509f978505ed0b7223f80c5fe71
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-02-05 15:10:30 +0300

    Essential Tools for MVC. Preparing the Example Project.

---
+++ EssentialTools/EssentialTools/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EssentialTools.Models;
+
+namespace EssentialTools.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        private Product[] products =
+        {
+            new Product { Name = "Canoe", Category = "Watersports", Price = 135000M },
+            new Product { Name = "Lifejacket", Category = "Watersports", Price = 270M },
+            new Product { Name = "Tennis ball", Category = "Tennis", Price = 13.50M },
+            new Product { Name = "Tennis net", Category = "Tennis", Price = 78.95M }
+        };
+        public ActionResult Index()
+        {
+            LinqValueCalculator calc = new LinqValueCalculator();
+            ShoppingCart cart = new ShoppingCart(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # anch_repository
 
+EssentialTools - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 6
+
 LanguageFeatures - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 4
 
 PartyInvites - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 2
