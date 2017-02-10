1d532699a4fe63217288f9e4aac6835cb2bc93d5
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-02-10 22:09:51 +0300

    Set Up the DI Container. (Adding NinjectDependencyResolver.cs. Set up NinjectWebCommon.cs)

---
+++ SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices (Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            // Put bindings here (Здесь размещаются привязки)
+        }
+    }
+}
