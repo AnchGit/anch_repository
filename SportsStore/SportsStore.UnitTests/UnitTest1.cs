8f369ed05863e66f858b07631b8b4485998abca6
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-02-11 13:55:30 +0300

    Creating TestMethod Can_Filter_Products

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -93,5 +93,32 @@ namespace SportsStore.UnitTests
             Assert.AreEqual(pagingInfo.TotalItems, 5);
             Assert.AreEqual(pagingInfo.TotalPages, 2);
         }
+
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            // Arrange
+            // Создание имитированного хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+                new Product { ProductID = 1, Name = "P2", Category = "Cat2" },
+                new Product { ProductID = 1, Name = "P3", Category = "Cat1" },
+                new Product { ProductID = 1, Name = "P4", Category = "Cat2" },
+                new Product { ProductID = 1, Name = "P5", Category = "Cat3" }
+            });
+            // Создание контроллера и установка размера страницы равным трем элементам
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Act
+            Product[] result = ((ProductsListViewModel)controller.List("Cat2", 1).Model).Products.ToArray();
+
+            // Assert
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
+        }
     }
 }
