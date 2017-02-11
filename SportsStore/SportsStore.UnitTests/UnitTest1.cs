7cae5ec4ebd3c30e3e69f344036704a1bd03bb20
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-02-11 21:39:10 +0300

    Adding TestMethod Can_Create_Categories

--- SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -120,5 +120,31 @@ namespace SportsStore.UnitTests
             Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
             Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
         }
+
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            // Arrange
+            // Создание имитированного хранилища
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Apples" },
+                new Product { ProductID = 2, Name = "P2", Category = "Apples" },
+                new Product { ProductID = 3, Name = "P3", Category = "Plums" },
+                new Product { ProductID = 4, Name = "P4", Category = "Oranges" },
+            });
+            // Создание контроллера
+            NavController target = new NavController(mock.Object);
+
+            // Act
+            string[] result = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+            // Assert
+            Assert.AreEqual(result.Length, 3);
+            Assert.AreEqual(result[0], "Apples");
+            Assert.AreEqual(result[1], "Oranges");
+            Assert.AreEqual(result[2], "Plums");
+        }
     }
 }
