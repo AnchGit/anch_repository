0803c138a1800c8070d3978551f58a9e88d9a3e1
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-02-12 13:05:42 +0300

    Creating partial view Summary.cshtml and adding it to the _Layout

--- SportsStore/SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -47,6 +47,11 @@ namespace SportsStore.WebUI.Controllers
             return RedirectToAction("Index", new { returnUrl });
         }
 
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+
         /* - заменен на CartModelBinder
         private Cart GetCart()
         {
