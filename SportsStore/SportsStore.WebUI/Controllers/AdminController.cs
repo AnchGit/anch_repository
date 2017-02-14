1a481379185aad268598f7860d0c0ad811eb5282
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-02-14 23:08:57 +0300

    Creating unit test AdminTests.cs and test method Index_Contains_All_Products

--- SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class AdminController : Controller
     {
-        // GET: Admin
-        public ActionResult Index()
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repo)
         {
-            return View();
+            repository = repo;
+        }
+        // GET: Admin
+        public ViewResult Index()
+        {
+            return View(repository.Products);
         }
     }
 }
