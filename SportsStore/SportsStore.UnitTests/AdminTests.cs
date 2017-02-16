be621f609f9711c05b347f4e17ae52b75290da85
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-02-16 19:22:45 +0300

    Adding test method Can_Delete_Valid_Products

--- SportsStore/SportsStore.UnitTests/AdminTests.cs
+++ SportsStore/SportsStore.UnitTests/AdminTests.cs
@@ -131,5 +131,31 @@ namespace SportsStore.UnitTests
             // Check the method result type
             Assert.IsInstanceOfType(result, typeof(ViewResult));
         }
+
+        [TestMethod]
+        public void Can_Delete_Valid_Products()
+        {
+            // Arrange
+            // Create a product
+            Product prod = new Product { Name = "Test", ProductID = 2 };
+            // Create the mock repository
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                prod,
+                new Product { ProductID = 3, Name = "P3" }
+            });
+            // Create the controller
+            AdminController target = new AdminController(mock.Object);
+
+            // Act
+            // Delete the product
+            target.Delete(prod.ProductID);
+
+            // Assert
+            // Ensure that the repository delete method was called with the correct product
+            mock.Verify(m => m.DeleteProduct(prod.ProductID));
+        }
     }
 }
