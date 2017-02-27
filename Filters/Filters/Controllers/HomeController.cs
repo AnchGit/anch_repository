a513bb50ee669eab5e5ba21b19099fad5925ff8b
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-02-28 00:43:20 +0300

    Adding Filters project

---
+++ Filters/Filters/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //[CustomAuth(false)]
+        [Authorize(Users = "admin")]
+        public string Index()
+        {
+            return "This is the Index action on the Home controller";
+        }
+    }
+}
--- README.md
+++ README.md
@@ -6,6 +6,8 @@ DebuggingDemo - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 14
 
 EssentialTools - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 6
 
+Filters - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 18
+
 LanguageFeatures - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 4
 
 PartyInvites - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 2
