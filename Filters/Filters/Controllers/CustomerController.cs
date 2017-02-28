fa3f76293750cdeace8cf825e472dd1c2257015d
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-03-01 00:32:02 +0300

    Filters (finalization).

---
+++ Filters/Filters/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+using Filters.Infrastructure;
+
+namespace Filters.Controllers
+{
+    [SimpleMessage(Message = "A")]
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        //[SimpleMessage(Message = "A", Order = 2)]
+        //[SimpleMessage(Message = "B", Order = 1)]
+        public string Index()
+        {
+            return "This is the Customer controller";
+        }
+
+        [CustomOverrideActionFilters]
+        [SimpleMessage(Message = "B")]
+        public string OtherAction()
+        {
+            return "This is the Other Action in the Customer controller";
+        }
+    }
+}
---
+++ Filters/Filters/Infrastructure/CustomActionAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    public class CustomActionAttribute : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsLocal)
+            {
+                filterContext.Result = new HttpNotFoundResult();
+            }
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+
+        }
+    }
+}
