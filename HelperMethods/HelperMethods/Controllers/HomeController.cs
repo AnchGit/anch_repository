c5af29cdb013f5c659fd2d1fde83a83629b1f1b0
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-03-07 23:38:05 +0300

    HelperMethods (finalization).

---
+++ HelperMethods/HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using HelperMethods.Models;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new string[] { "Apple", "Orange", "Pear" };
+            ViewBag.Cities = new string[] { "New York", "London", "Minsk" };
+            string message = "This is an HTML element: <input>"; 
+            return View((object)message);
+        }
+
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person person)
+        {
+            return View(person);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,6 +10,8 @@ EssentialTools - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 6
 
 Filters - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 18
 
+HelperMethods - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 21
+
 LanguageFeatures - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 4
 
 PartyInvites - Adam Freeman Pro ASP .Net MVC 5 (2013): Chapter 2
