e3ada7c652cd82ee3610ea6155358e437b5c0547
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-03-11 22:15:47 +0300

    HelperMethods (finalization).

--- HelperMethods/HelperMethods/Models/Person.cs
+++ HelperMethods/HelperMethods/Models/Person.cs
@@ -1,14 +1,28 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace HelperMethods.Models
 {
-    public class Person
+    //[DisplayName("New Person")]
+    [MetadataType(typeof(PersonMetaData))]
+    public partial class Person
     {
+        //[HiddenInput]
+        //[HiddenInput(DisplayValue = false)]
+        //[ScaffoldColumn(false)]
         public int PersonId { get; set; }
+        //[Display(Name = "First")]
+        //[UIHint("MultilineText")]
         public string FirstName { get; set; }
+        //[Display(Name = "Last")]
         public string LastName { get; set; }
+        //[Display(Name = "Birth Date")]
+        //[DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
         public Address HomeAddress { get; set; }
+        //[Display(Name = "Approved")]
         public bool IsApproved { get; set; }
         public Role Role { get; set; }
     }
