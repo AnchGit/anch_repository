28cd38607af7d45251d97360caf217897d54bdfb
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-03-15 00:52:11 +0300

    ModelValidation. Using Alternative Validation Techniques.

--- ModelValidation/ModelValidation/Models/Appointment.cs
+++ ModelValidation/ModelValidation/Models/Appointment.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using ModelValidation.Infrastructure;
 
 namespace ModelValidation.Models
 {
+    [NoJoeOnMondays]
     public class Appointment
     {
+        [Required]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
+        //[Required(ErrorMessage = "Please enter a date")]
+        [FutureDate(ErrorMessage = "Please enter a date in the future")]
         public DateTime Date { get; set; }
 
+        //[Range(typeof(bool), "true", "true", ErrorMessage = "You must accept the terms")]
+        [MustBeTrue(ErrorMessage = "You must accept the terms")]
         public bool TermsAccepted { get; set; }
     }
 }
