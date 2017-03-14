ae41160e8d1636b807c8ff955aaf4966652275a6
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-03-15 00:19:31 +0300

    ModelValidation. Displaying Validation Messages.

---
+++ ModelValidation/ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Web.Mvc;
+using ModelValidation.Models;
+
+namespace ModelValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { Date = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt)
+        {
+            if (string.IsNullOrEmpty(appt.ClientName))
+            {
+                ModelState.AddModelError("ClientName", "Please enter your name");
+            }
+            if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date) 
+            {
+                ModelState.AddModelError("Date", "Please enter a date in the future");
+            }
+            if (!appt.TermsAccepted)
+            {
+                ModelState.AddModelError("TermsAccepted", "You must accept the terms");
+            }
+
+            if (ModelState.IsValidField("ClientName")  && ModelState.IsValidField("Date") 
+                && appt.ClientName == "Joe" && appt.Date.DayOfWeek == DayOfWeek.Monday)
+            {
+                ModelState.AddModelError("", "Joe cannot book appointments on Mondays");
+            }
+
+            if (ModelState.IsValid)
+            {
+                // В рельном проекте здесь находились бы операторы для сохранения нового объекта Appointment в хранилище
+                return View("Completed", appt);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
