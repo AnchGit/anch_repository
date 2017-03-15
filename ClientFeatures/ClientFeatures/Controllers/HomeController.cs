de5a510c70cfb88c25458bd895e9e42645efb5b4
alexander.stepanov <aleksandr.stepanov@gmail.com>
2017-03-15 20:31:55 +0300

    Adding new project ClientFeatures.

---
+++ ClientFeatures/ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Mvc;
+using ClientFeatures.Models;
+
+namespace ClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment { ClientName = "David", TermsAccepted = true });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appt)
+        {
+            return Json(appt, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
